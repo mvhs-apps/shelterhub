f032266c13f7b05b75fd24b2cdab904c00d803b2
sfreimuth <suneelfreimuth@yahoo.com>
2018-02-25 11:10:55 -0800

    Minor edits

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 
-<<<<<<< HEAD
 import SignInPage from './components/sign-in-page';
 
-=======
-import NavBar from './components/nav-bar';
-import SignInPanel from './components/sign-in-panel';
->>>>>>> f28a21b79a20ec7126828c92c32e28633965f3b3
 import Fire from './firebase.js';
 
 const database = Fire.database();
@@ -21,7 +16,7 @@ export default class App extends Component {
   render() {
     return(
       <div className="page-body">
-        <SignInPage />
+        <SignInPage createUser={this.createUser} />
       </div>
     );
   }
--- src/components/sign-in-page.js
+++ src/components/sign-in-page.js
@@ -4,9 +4,11 @@ import SignInPanel from './sign-in-panel';
 
 export default class SignInPage extends Component {
   render() {
-    <div>
-      <NavBar />
-      <SignInPanel />
-    </div>
+    return(
+      <div>
+        <NavBar />
+        <SignInPanel createUser={this.props.createUser} />
+      </div>
+    );
   }
 }
