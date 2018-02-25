0d7f0d3505ff4116491d0f79a199d8a9f914e319
Joshua Buhain <jbuhain@Emmanuels-MacBook-Pro.local>
2018-02-25 11:14:16 -0800

    edit - josh

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 
-<<<<<<< HEAD
 import SignInPage from './components/sign-in-page';
 
-=======
-import NavBar from './components/nav-bar';
-import SignInPanel from './components/sign-in-panel';
->>>>>>> f28a21b79a20ec7126828c92c32e28633965f3b3
 import Fire from './firebase.js';
 
 const database = Fire.database();
