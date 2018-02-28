f50a4901f4bec8c32b417aa61f679869473ae092
Jeremy Hsu <jeremyhsu1@gmail.com>
2018-02-27 19:29:56 -0800

    Minor change regarding home

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import SignInPage from './components/sign-in-page';
 import About from './components/about';
 import ContactUs from './components/contactUs';
+import Home from './components/home';
 
 ReactDOM.render(
  <BrowserRouter> 
@@ -14,6 +15,7 @@ ReactDOM.render(
         <Route path="/signin" exact component = {SignInPage} />
         <Route path="/about" exact component ={About} />
         <Route path="/contactus" exact component ={ContactUs} />
+        <Route path="/home" exact component ={Home} />
     </Switch>
 </BrowserRouter>
 ,document.getElementById('root'));
