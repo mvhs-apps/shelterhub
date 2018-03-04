4f907930774ab3ce0b33610eba35e9da17bd2988
Jeremy Hsu <jeremyhsu1@gmail.com>
2018-03-04 09:58:33 -0800

    Visual changes to About page

--- src/components/about.js
+++ src/components/about.js
@@ -7,8 +7,11 @@ export default class About extends Component {
       <div>
         <NavBar />
        <h1>
-           about page
+           About Us
        </h1>
+       <br />
+       <h3>ShelterHub strives to bring homeless shelters closer together through the integrated use of technology.</h3>
+       <h3>Here, you will be able to communicate and connect with many other shelters in your local community.</h3>
       </div>
     );
   }
