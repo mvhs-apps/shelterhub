6232eb2e469e527a4274017d8c548855a8989040
Patrick Brown <patrickbrown1312@gmail.com>
2018-04-09 12:18:27 -0700

    Added some conditional rendering to profile-panel.js, but the sign-up doesnt add anything to the authentication firebase, and the sign-up works no matter what you put in

--- src/components/profile-panel.js
+++ src/components/profile-panel.js
@@ -8,20 +8,29 @@ export default class ProfilePanel extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        user:auth.currentUser,
-        email:'',
-        username:''
+        user:auth.currentUser
     };
+    this.UserRender = this.UserRender.bind(this);
+    this.GuestRender = this.GuestRender.bind(this);
+  }
+  UserRender() {
+    return <div className='panel_body'>Welcome back!</div>;
+  }
+
+  GuestRender() {
+    return <div className='panel_body'>Please sign up.</div>;
   }
   render() {
     return(
       <div className='profile-panel'>
         <div className='panel_header'>
           <h1>Profile</h1>
-        </div>
-       {/*<div className='panel_body'>
-            Email: {this.state.user.username}
-        </div>*/}
+        </div>  
+        {this.state.user ? (
+          this.UserRender()
+        ) : (
+          this.GuestRender()
+        )}
       </div>
     );
   }
