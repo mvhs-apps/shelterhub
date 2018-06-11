3b391588dc10ac1e511f2fb88f9971d3178968b8
Jeremy Hsu <jeremyhsu1@gmail.com>
2018-06-11 12:46:04 -0700

    Imported Link in Home

--- src/components/home-page/home.js
+++ src/components/home-page/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from '../nav-bar';
+import { Link } from 'react-router-dom'; 
 export default class Home extends Component {
   constructor() {
     super();
