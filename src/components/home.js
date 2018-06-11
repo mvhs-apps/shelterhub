7832def3e6f9996b89b277878a227fbaf57fba09
Jeremy Hsu <jeremyhsu1@gmail.com>
2018-06-11 12:35:14 -0700

    Home content

--- src/components/home.js
+++ src/components/home.js
@@ -12,6 +12,10 @@ export default class Home extends Component {
       <div>
         <NavBar />
 
+        <h1>Welcome to ShelterHub!</h1>
+
+        <br />
+
         <p>ShelterHub is a nonprofit organization that strives 
           to provide an easy, intuitive user interface for homeless 
           shelters and others to communicate with each other.</p>
@@ -25,9 +29,14 @@ export default class Home extends Component {
           (but not limited to) pictures, reviews, and contact 
           information.</p>
 
-        <p>ShelterHub is a nonprofit organization that strives to provide an easy, intuitive user interface for homeless shelters and others to communicate with each other.</p>
-        <p>Homeless shelters would create a ShelterHub account, thus granting them access to other shelters' contact information, inventory totals, and other relevant details.</p> 
-        <p>However, ShelterHub is not confined to purely homeless shelter accounts. Without signing up, you are free to read our up-to-date information about the various homeless shelters and programs currently on ShelterHub, including (but not limited to) pictures, reviews, and contact information.</p>
+        <br />
+        <br />
+        
+        <p>Learn more about ShelterHub {<Link to='/about'>here</Link>}.</p>
+
+        <br />
+
+        <p>Contact us {<Link to='contactus'>here</Link>}.</p>
       </div>
     );
   }
