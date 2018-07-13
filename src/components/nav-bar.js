ae151feff10590d55835d8f1448396e740fb0838
Jeremy Hsu <jeremyhsu1@gmail.com>
2018-07-12 21:46:24 -0700

    More visual tweaks

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 export default class NavBar extends Component {
   render() {
     return(
       <div>
         <ul>
-          <li className="nav-bar_title"><a href="/Home">ShelterHub</a></li>
-          <li className="nav-bar_item"><a href="/Settings">Settings</a></li>
-          <li className="nav-bar_item"><a href="/About">About</a></li>
-          <li className="nav-bar_item"><a href="/ContactUs">Contact</a></li>
-          <li className="nav-bar_item"><a href="/Profile">Profile</a></li>
-          <li className="nav-bar_item"><a href="/Database">Inventory</a></li>
+          <li className="nav-bar_title"><Link to={""}>ShelterHub</Link></li>
+          <li className="nav-bar_item"><Link to={"/settings"}>Settings</Link></li>
+          <li className="nav-bar_item"><Link to={"/about"}>About</Link></li>
+          <li className="nav-bar_item"><Link to={"/contactus"}>Contact</Link></li>
+          <li className="nav-bar_item"><Link to={"/profile"}>Profile</Link></li>
+          <li className="nav-bar_item"><Link to={"/database"}>Inventory</Link></li>
         </ul>
       </div>
     );
